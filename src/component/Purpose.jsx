b9b2566d444f89d549c106b4b9f2b0463d8dd06a
Sakib Hossain <sakibs.ngn@gmail.com>
2024-09-17 15:12:10 +0600

    two7

--- src/component/Purpose.jsx
+++ src/component/Purpose.jsx
@@ -6,16 +6,16 @@ const Purpose = () => {
         <section className='bg-[url("../src/assets/background/seamless.png")] bg-cover bg-center bg-no-repeat py-20'>
             <Container className='px-3'>
                 <div className="flex justify-between items-center">
-                    <div className="w-[45%] border-[1px] border-[#000] bg-white py-20 px-10">
+                    <div className="w-[45%] border-[1px] border-[#000] bg-[#f5f5f5fd] py-20 px-10">
                         <h4 className="font-suse font-semibold text-md text-[#083da0d6] uppercase tracking-widest pb-8">Purpose</h4>
                         <p className="font-suse font-bold text-4xl uppercase leading-[45px]">To improve the standard of living of our customers and make them happy. </p>
                     </div>
-                    <div className="w-[45%] border-[1px] border-[#000] bg-white py-20 px-10">
+                    <div className="w-[45%] border-[1px] border-[#000] bg-[#f5f5f5fd] py-20 px-10">
                         <h4 className="font-suse font-semibold text-md text-[#083da0d6] uppercase tracking-widest pb-8">Vision</h4>
                         <p className="font-suse font-bold text-4xl uppercase leading-[45px]">To become the most trusted and respected real estate company in Bangladesh.</p>
                     </div>
                 </div>
-                <div className="mt-20 border-[1px] border-[#000] bg-white py-20 px-10">
+                <div className="mt-20 border-[1px] border-[#000] bg-[#f5f5f5fd] py-20 px-10">
                     <h4 className="font-suse font-semibold text-md text-[#083da0d6] uppercase tracking-widest pb-8">OUR VALUES</h4>
                     <div className="flex justify-between">
                         <h6 className="font-suse font-medium text-[16px] text-[#000]">Integrity</h6>
